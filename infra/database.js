9881017aaf74f82b5b843873dcbf186b57543500
José Vitor <josevitorbarroso123@gmail.com>
2026-03-11 06:40:21 -0300

    Adicionando arquivos para o repositorio

---
+++ infra/database.js
@@ -0,0 +1,37 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSlValues(),
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+};
+
+function getSSlValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,3 @@
+export default async function migrations(request, response) {
+  response.status(200).json([]);
+}
