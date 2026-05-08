16aa3ab8a00ee750163c1337cfbb8b14a86280ea
José Vitor <113712822+vitaa1@users.noreply.github.com>
2026-05-08 11:55:02 +0000

    fix: corrigindo formatação

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import "./globals.css";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export default function RootLayout({
   children,
--- types/global.d.ts
+++ types/global.d.ts
@@ -4,4 +4,3 @@ declare module "*.css" {
 }
 
 export {};
-
